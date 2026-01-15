d94f8611fbe39fc762aa10fea77f093bd75eaea2
YBDVitto <vittobervr@gmail.com>
2026-01-15 22:30:20 +0100

    Filtering products

--- frontend/js/utils/filterProducts.js
+++ frontend/js/utils/filterProducts.js
@@ -3,7 +3,7 @@ import renderProducts from "./renderProducts.js"
 
 const fetchFilteredProducts = async (category) => {
     try {
-        const result = await fetch(`https://jjtd4cc3icl3gqbugqmw63m2xq0mxohx.lambda-url.us-east-1.on.aws/shop/filter?category=${category}`, {
+        const result = await fetch(`https://jjtd4cc3icl3gqbugqmw63m2xq0mxohx.lambda-url.us-east-1.on.aws/shop/filter?category=${encodeURIComponent(category)}`, {
             method: 'GET',
         })
         const data = await result.json()
